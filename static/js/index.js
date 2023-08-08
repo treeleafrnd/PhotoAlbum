2dffba1ccaf3501d485d6d870cbf875a7f6bf94f
Ruja Awal <awalruja2016@gmail.com>
2023-08-08 12:01:12 +0545

    Preview images and make cancellable

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,6 @@ function preview(event) {
 
   // append new value to the array
 
-  console.log(x);
   var mainDiv = document.getElementById("image-preview");
   for (i = 0; i < elems.files.length; i++) {
     var file = elems.files[i];
@@ -21,32 +20,46 @@ function preview(event) {
     imageDiv.id = "img-" + i;
     var image = document.createElement("img");
     var url = URL.createObjectURL(event.target.files[i]);
-    console.log(url);
+    // console.log(url);
     image.src = url;
     image.style.width = "200px";
     image.style.height = "80px";
     mainDiv.append(imageDiv);
     imageDiv.append(image);
-    var button =
-      "<button class='btn btn-primary' onclick = removeImage(" +
-      i +
-      ")>Close</button>";
-    // var button = document.createElement("button");
-    // button.className = "btn btn-primary";
 
-    // button.innerHTML = "Close";
-    // button.id = i;
+    var button = document.createElement("div");
+
+    button.innerHTML = "Close";
+    button.className = "btn btn-secondary";
+    button.id = i;
+    imageDiv.append(button);
+    image1 = document.getElementById("img-" + i);
+    console.log(image1);
+    // console.log("img-" + i);
+    // j = button.id;
+    console.log(i, 112341);
+    button.onclick = function removeImage(i) {
+      console.log(i, 7, 8);
+      var image_to_delete = document.getElementById(
+        "img-" + i.srcElement.firstChild.parentNode.id
+      );
+      console.log(image_to_delete);
+      mainDiv.removeChild(image_to_delete);
+      console.log(i.srcElement.firstChild.parentNode.id);
+    };
+
     // button.onclick = () => {
 
     // };
-
-    imageDiv.innerHTML(button);
 
     // imageDiv.remove(image);
     // b.onclick = function() { alert('OnClick'); }
   }
 }
 
+// function removeImage(x) {
+//   console.log(x);
+// }
 var inputFile = document.getElementById("brand_logo");
 removeImg = () => {
   document.getElementById("frame").setAttribute("src", "");
